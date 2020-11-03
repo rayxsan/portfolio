becc1f21a0e6e65f0b9094ffb5b650eea907103b
Yandy Gonzalez <yandygonzalez@gmail.com>
2020-11-03 17:23:14 -0500

    add form validation

--- src/Components/Form/FormF.tsx
+++ src/Components/Form/FormF.tsx
@@ -1,12 +1,5 @@
 import React, { FunctionComponent } from "react";
-import {
-  Formik,
-  Field,
-  Form,
-  FormikHelpers,
-  ErrorMessage,
-  useField,
-} from "formik";
+import { Formik, Field, Form, FormikHelpers, ErrorMessage, useField } from "formik";
 import Button from "../Button/Button";
 import * as Yup from "yup";
 import StyledFormF from "./FormF.styled";
@@ -21,9 +14,6 @@ interface Values {
   password?: string;
   confirmPassword?: string;
   gender?: string;
-  checked?: boolean;
-  toggle?: boolean;
-  error?: boolean;
 }
 
 interface LiveFeedback {
@@ -35,12 +25,7 @@ interface LiveFeedback {
 // Async Validation
 // const sleep = (ms: any) => new Promise(resolve => setTimeout(resolve, ms));
 
-const TextInputLiveFeedback = ({
-  name,
-  id,
-  placeholder,
-  ...props
-}: LiveFeedback) => {
+const TextInputLiveFeedback = ({ name, id, placeholder, ...props }: LiveFeedback) => {
   const [field, meta] = useField(name);
 
   // Show inline feedback if EITHER
@@ -48,8 +33,7 @@ const TextInputLiveFeedback = ({
   // - or, the has been visited (touched === true)
   const [didFocus, setDidFocus] = React.useState(false);
   const handleFocus = () => setDidFocus(true);
-  const showFeedback =
-    (!!didFocus && field.value.trim().length > 2) || meta.touched;
+  const showFeedback = (!!didFocus && field.value.trim().length > 2) || meta.touched;
 
   return (
     <div>
@@ -64,47 +48,45 @@ const TextInputLiveFeedback = ({
         onFocus={handleFocus}
         error={meta.error}
       />
-      {meta.error && showFeedback ? (
-        <ErrorMessage component="span" name={name} />
-      ) : null}
+      {meta.error && showFeedback ? <ErrorMessage component="span" name={name} /> : null}
     </div>
   );
 };
 
+// Yup API: https://github.com/jquense/yup
 const SimpleFormValidation = Yup.object().shape({
-  username: Yup.string()
-    .min(4, "Too Short!")
-    .max(9, "Too Long!")
-    .required("Required"),
+  username: Yup.string().required("Required").min(4, "Too Short!").max(9, "Too Long!"),
   firstName: Yup.string().required("Required"),
   email: Yup.string().email("Invalid email").required("Required"),
   date: Yup.date(),
   creditCard: Yup.number(),
   mobileNumber: Yup.number(),
-  password: Yup.string(),
-  confirmPassword: Yup.string(),
+  password: Yup.string()
+    .required("No password provided.")
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+  confirmPassword: Yup.string().oneOf([Yup.ref("password"), undefined], "Passwords must match"),
 });
 
 // TODO Style Validation
 const FormF: FunctionComponent<Values> = () => {
+  const initialValues: Values = {
+    username: "",
+    firstName: "",
+    email: "",
+    date: "",
+    creditCard: "",
+    mobileNumber: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  };
   return (
     <StyledFormF>
       <Formik
-        initialValues={{
-          username: "",
-          firstName: "",
-          email: "",
-          date: "",
-          creditCard: "",
-          mobileNumber: "",
-          password: "",
-          confirmPassword: "",
-          gender: "",
-          checked: false,
-          toggle: false,
-          error: false,
-        }}
+        initialValues={initialValues}
         validationSchema={SimpleFormValidation}
+        validateOnChange={true}
         onSubmit={(values, { setSubmitting }: FormikHelpers<Values>) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
@@ -120,32 +102,19 @@ const FormF: FunctionComponent<Values> = () => {
                 name="username"
                 placeholder="Username (Min length 4, Max length 9)"
               />
-              <TextInputLiveFeedback
-                id="firstName"
-                name="firstName"
-                placeholder="First Name"
-              />
-              <TextInputLiveFeedback
-                id="email"
-                name="email"
-                placeholder="Email"
-              />
+              <TextInputLiveFeedback id="firstName" name="firstName" placeholder="First Name" />
+              <TextInputLiveFeedback id="email" name="email" placeholder="Email" />
+              {/* TODO: use input of type date */}
               <TextInputLiveFeedback id="date" name="date" placeholder="Date" />
-              <TextInputLiveFeedback
-                id="creditCard"
-                name="creditCard"
-                placeholder="Credit Card"
-              />
+              <TextInputLiveFeedback id="creditCard" name="creditCard" placeholder="Credit Card" />
               <TextInputLiveFeedback
                 id="mobileNumber"
                 name="mobileNumber"
                 placeholder="Mobile Number"
               />
-              <TextInputLiveFeedback
-                id="password"
-                name="password"
-                placeholder="Password"
-              />
+              {/* TODO: use input of type password */}
+              <TextInputLiveFeedback id="password" name="password" placeholder="Password" />
+              {/* TODO: use input of type password */}
               <TextInputLiveFeedback
                 id="confirmPassword"
                 name="confirmPassword"
@@ -169,8 +138,8 @@ const FormF: FunctionComponent<Values> = () => {
 
               <div role="group" aria-labelledby="checkbox-group">
                 <label htmlFor="toggle">
-                  <Field type="checkbox" name="toggle" />I have read and agree
-                  to the terms of service.
+                  <Field type="checkbox" name="toggle" />I have read and agree to the terms of
+                  service.
                 </label>
               </div>
             </section>
