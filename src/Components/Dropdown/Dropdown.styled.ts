6ef300976c999c25c2264e7d3998a11c309272b4
Yandy Gonzalez <yandygonzalez@gmail.com>
2020-11-03 17:21:09 -0500

    add form

--- src/Components/Dropdown/Dropdown.styled.ts
+++ src/Components/Dropdown/Dropdown.styled.ts
@@ -1,18 +1,46 @@
 import styled from "styled-components";
 
 export const StyledDropdown = styled.div`
-  label,
-  footer {
-    font-family: sans-serif;
+  color: black;
+
+  // remove picker indicator and focus outline from input
+  input::-webkit-calendar-picker-indicator {
+    display: none;
+  }
+  *:focus {
+    outline: none;
   }
 
-  label {
-    font-size: 1rem;
-    padding-right: 10px;
+  datalist {
+    background-color: #fff;
   }
 
-  select {
-    font-size: 0.9rem;
-    padding: 2px 5px;
+  input {
+    height: 2rem;
+    width: fit-content;
+    border: 0;
+    border-radius: 0.4rem;
+    background-color: #bfbfbf;
+    opacity: 0.7;
+    text-align: center;
+    :hover,
+    :focus {
+      opacity: 1;
+    }
+    ::placeholder {
+      /* Chrome, Firefox, Opera, Safari 10.1+ */
+      color: #000;
+      opacity: 1; /* Firefox */
+    }
   }
+
+  .jude {
+    background-color: pink;
+    display: table;
+  }
+
+  // option {
+  //   font-size: 0.9rem;
+  //   padding: 2px 5px;
+  // }
 `;
--- src/Components/Dropdown/Dropdown.tsx
+++ src/Components/Dropdown/Dropdown.tsx
@@ -1,27 +1,56 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect, useRef, SyntheticEvent } from "react";
+import DropdownCard from "./DropdownCard";
 import { StyledDropdown } from "./Dropdown.styled";
 
 interface Props {
   options: string[];
   defaultOption?: string;
-  inputVal?: boolean;
+  // inputVal?: boolean;
   label?: string;
   placeholder?: string;
 }
 
 // TODO styling
 // Make the input clearable
-const Dropdown: FunctionComponent<Props> = ({ options, placeholder, inputVal, label }) => {
+const Dropdown: FunctionComponent<Props> = ({ options, placeholder, label }) => {
+  const [open, setOpen] = useState(false);
+
+  // <div> reference type
+  const divRef = useRef<HTMLInputElement>(null);
+
+  const useOutsideAlert = (ref: HTMLInputElement): void => {
+    console.log("ref", ref);
+
+    // useEffect(() => {
+    //   const handleClickOutside = (event: SyntheticEvent) => {
+    //     if (ref.current && !ref.current.contains(event.target)) {
+    //       setOpen(false);
+    //     }
+    //   };
+
+    //   document.addEventListener("mousedown", handleClickOutside);
+    //   return () => {
+    //     // Unbind the event listener on clean up
+    //     document.removeEventListener("mousedown", handleClickOutside);
+    //   };
+    // }, [ref]);
+  };
+
   return (
     <StyledDropdown>
-      <label htmlFor="choice">{label}</label>
-      {/* input's list has to be equal to list's id */}
+      <div ref={divRef} className="jude">
+        <input list="input-list" placeholder={placeholder} onFocus={() => setOpen(true)} />
+        {open && <DropdownCard data={options}></DropdownCard>}
+
+        {/* <label htmlFor="choice">{label}</label>
+      input's list has to be equal to list's id
       {inputVal && <input list="input-list" placeholder={placeholder} />}
       <datalist id="input-list">
         {options.map(option => {
           return <option value={option}>{option}</option>;
         })}
-      </datalist>
+      </datalist> */}
+      </div>
     </StyledDropdown>
   );
 };
