e283fc1e9e054eef931ef1eeb683c8c4048fa884
Raymel Manreza <raymel.manreza@gmail.com>
2020-12-06 12:11:18 -0500

    Squashed commit of the following:
    commit a8952c5a9f3df7369bd30d101ffcc4114e2ea709
    Author: Raymel Manreza <raymel.manreza@gmail.com>
    Date:   Sun Dec 6 12:06:10 2020 -0500
    
        Basic table
    
    commit bf094d907c5439b16b9048a9eb2acbbaa53a0dd3
    Author: Raymel Manreza <raymel.manreza@gmail.com>
    Date:   Sat Dec 5 20:15:16 2020 -0500
    
        table work in progress
    
    commit 2fee583bc84a65bc82b181b268e5efcddfaa737e
    Author: Raymel Manreza <raymel.manreza@gmail.com>
    Date:   Mon Nov 30 20:10:00 2020 -0500
    
        Table work in progress
    
    commit efee123dbff0494e5121229724585315a009519b
    Author: Raymel Manreza <raymel.manreza@gmail.com>
    Date:   Thu Nov 26 12:19:15 2020 -0500
    
        Table work in progress

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -3,21 +3,36 @@ import StyledTable from "./Table.styled";
 
 interface TableProps {
   header?: any[];
-  body?: any[];
+  rows?: any[][];
 }
 let rows = ["Setting One", "Setting Two", "Setting Three", "Setting Four"];
 class Table extends Component<TableProps> {
-  createTable = () => {
-    let table = [];
-    for (let i = 0; i < rows.length; i++) {
-      let children = [];
-      children.push(<td>{rows[i]}</td>, <td>ListMEnu</td>);
-      table.push(<tr>{children}</tr>);
-    }
-    return table;
-  };
+  renderTableHeader() {
+    const header = this.props.header;
+    const headings = header!.map((head) => <th>{head}</th>);
+    return <tr>{headings}</tr>;
+  }
+
+  renderTableBody() {
+    const rows = this.props.rows;
+    const tbody = rows!.map((row) => {
+      return (
+        <tr>
+          {row.map((row) => (
+            <td>{row}</td>
+          ))}
+        </tr>
+      );
+    });
+    return tbody;
+  }
   render() {
-    return <StyledTable>{this.createTable()}</StyledTable>;
+    return (
+      <StyledTable>
+        <thead>{this.renderTableHeader()}</thead>
+        <tbody>{this.renderTableBody()}</tbody>
+      </StyledTable>
+    );
   }
 }
 
--- src/Pages/TablePage.tsx
+++ src/Pages/TablePage.tsx
@@ -5,10 +5,76 @@ import Table from "../Components/Table/Table";
 
 const StyledTablePage = styled.div``;
 
+const headings = [
+  "Product name",
+  "SKU",
+  "Stock quantity",
+  "Wholesale cost",
+  "Sale price",
+  "Quantity sold",
+  "Gross sales",
+  "Net sales",
+  "Notes",
+];
+
+const rows = [
+  [
+    "Red and black plaid scarf with thin red stripes and thick black stripes",
+    124689325,
+    28,
+    "$35.00",
+    "$60.00",
+    12,
+    "$720.00",
+    "$300.00",
+    "",
+  ],
+  [
+    "Yellow plaid scarf",
+    124689389,
+    0,
+    "$35.00",
+    "$60.00",
+    20,
+    "$1200.00",
+    "$500.00",
+    "Currently on back order by the supplier. Do not place another order to restock.",
+  ],
+  [
+    "Blue plaid scarf",
+    124689332,
+    30,
+    "$35.00",
+    "$60.00",
+    10,
+    "$600.00",
+    "$250.00",
+    "",
+  ],
+  [
+    "Pink plaid scarf",
+    124689376,
+    16,
+    "$35.00",
+    "$60.00",
+    4,
+    "$240.00",
+    "$100.00",
+    "",
+  ],
+];
+
+const header1 = [1, 2, 3];
+const row1 = [
+  ["a", "b", "c"],
+  ["a", "b", "c"],
+  ["a", "b", "c"],
+];
+
 const TablePage = () => (
   <StyledTablePage>
     <Card title="Basic Table">
-      <Table />
+      <Table header={headings} rows={rows} />
     </Card>
   </StyledTablePage>
 );
