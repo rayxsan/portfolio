201fa30c30aa0f6bc8a274ac462b6396add4ef0e
Raymel Manreza <raymel.manreza@gmail.com>
2020-12-06 12:11:18 -0500

    Squashed commit of the following:
    commit a8952c5a9f3df7369bd30d101ffcc4114e2ea709
    Author: Raymel Manreza <raymel.manreza@gmail.com>
    Date:   Sun Dec 6 12:06:10 2020 -0500
    
        Basic table
    
    commit bf094d907c5439b16b9048a9eb2acbbaa53a0dd3
    Author: Raymel Manreza <raymel.manreza@gmail.com>
    Date:   Sat Dec 5 20:15:16 2020 -0500
    
        table work in progress
    
    commit 2fee583bc84a65bc82b181b268e5efcddfaa737e
    Author: Raymel Manreza <raymel.manreza@gmail.com>
    Date:   Mon Nov 30 20:10:00 2020 -0500
    
        Table work in progress
    
    commit efee123dbff0494e5121229724585315a009519b
    Author: Raymel Manreza <raymel.manreza@gmail.com>
    Date:   Thu Nov 26 12:19:15 2020 -0500
    
        Table work in progress

--- src/Components/Table/Table.styled.tsx
+++ src/Components/Table/Table.styled.tsx
@@ -1,5 +1,29 @@
 import styled from "styled-components";
 
-const StyledTable = styled.table``;
+const StyledTable = styled.table`
+  text-align: left;
+  cursor: default;
+  border-collapse: collapse;
+  width: 100%;
+
+  td,
+  th {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+  tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+  tr:hover {
+    background-color: #ddd;
+  }
+
+  th {
+    background-color: ${({ theme }) => theme.primaryColor};
+    color: white;
+    padding-top: 12px;
+    padding-bottom: 12px;
+  }
+`;
 
 export default StyledTable;
