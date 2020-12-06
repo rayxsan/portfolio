154b870c92c13aa826b00b2e479028e4ba87e6c7
Raymel Manreza <raymel.manreza@gmail.com>
2020-12-06 12:53:17 -0500

    Table types defined

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -2,29 +2,34 @@ import React, { Component } from "react";
 import StyledTable from "./Table.styled";
 
 interface TableProps {
-  header?: any[];
-  rows?: any[][];
+  header?: (string | number)[];
+  rows?: (string | number)[][];
 }
-let rows = ["Setting One", "Setting Two", "Setting Three", "Setting Four"];
+
 class Table extends Component<TableProps> {
   renderTableHeader() {
     const header = this.props.header;
-    const headings = header!.map((head) => <th>{head}</th>);
-    return <tr>{headings}</tr>;
+    if (header) {
+      const headings = header.map((head) => <th>{head}</th>);
+
+      return <tr>{headings}</tr>;
+    }
   }
 
   renderTableBody() {
     const rows = this.props.rows;
-    const tbody = rows!.map((row) => {
-      return (
-        <tr>
-          {row.map((row) => (
-            <td>{row}</td>
-          ))}
-        </tr>
-      );
-    });
-    return tbody;
+    if (rows) {
+      const tbody = rows!.map((row) => {
+        return (
+          <tr>
+            {row.map((row) => (
+              <td>{row}</td>
+            ))}
+          </tr>
+        );
+      });
+      return tbody;
+    }
   }
   render() {
     return (
