284f9475d0f9357ea21cb49807ac3427ea42aee7
Raymel Manreza <raymel.manreza@gmail.com>
2021-01-18 21:55:01 -0500

    Axios installed. Table fetch from backend work in progress

--- src/Pages/TablePage.tsx
+++ src/Pages/TablePage.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import Card from "../Components/UI/Card/Card";
 import Table from "../Components/Table/Table";
 import { data1, data2 } from "../shared/tableData";
+import axios from "axios";
 
 const StyledTablePage = styled.div``;
 
@@ -17,15 +18,39 @@ const headers = {
   university: "University",
 };
 
-const TablePage = () => (
-  <StyledTablePage>
-    <Card title="Basic Table">
-      <Table data={data2} headers={headers} />
-    </Card>
-    <Card title="Pagination Table">
-      <Table data={data1} showPages headers={headers} />
-    </Card>
-  </StyledTablePage>
-);
+interface Props {}
+interface State<DataItem> {
+  data: DataItem[];
+}
+class TablePage extends Component<Props, State<any>> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/albums")
+      .then((response) => this.setState({ data: response.data }));
+  }
+
+  render() {
+    return (
+      <StyledTablePage>
+        <Card title="Basic Table">
+          <Table data={data2} headers={headers} />
+        </Card>
+        <Card title="Pagination Table">
+          <Table data={data1} showPages headers={headers} />
+        </Card>
+        <Card title="Table fetching from backend">
+          <Table data={this.state.data} showPages />
+        </Card>
+      </StyledTablePage>
+    );
+  }
+}
 
 export default TablePage;
