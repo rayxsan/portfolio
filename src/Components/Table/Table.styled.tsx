d5258fff5bdeb7921ac54735155c53e58681a660
Raymel Manreza <raymel.manreza@gmail.com>
2021-01-20 10:17:55 -0500

    Table solution for mobile view implemented

--- src/Components/Table/Table.styled.tsx
+++ src/Components/Table/Table.styled.tsx
@@ -36,6 +36,7 @@ export const StyledTable = styled.div<styledProps>`
 	*/
   @media only screen and (max-width: 760px),
     (min-device-width: 768px) and (max-device-width: 1024px) {
+    font-size: 100%;
     /* Force table to not be like tables anymore */
     table,
     thead,
@@ -46,45 +47,22 @@ export const StyledTable = styled.div<styledProps>`
       display: block;
     }
 
-    /* Hide table headers (but not display: none;, for accessibility) */
     thead tr {
-      position: absolute;
-      top: -9999px;
-      left: -9999px;
+      box-sizing: border-box;
+      border: 1px solid black;
     }
 
     tr {
       margin: 0 0 1rem 0;
-    }
-
-    tr:nth-child(odd) {
-      background: #ccc;
+      border: 1px solid #ddd;
     }
 
     td {
       /* Behave  like a "row" */
       border: none;
-      border-bottom: 1px solid #ddd;
       position: relative;
       padding-left: 50%;
     }
-
-    td:before {
-      /* Now like a table header */
-      position: absolute;
-      /* Top/left values mimic padding */
-      top: 0;
-      left: 6px;
-      width: 45%;
-      padding-right: 10px;
-      white-space: nowrap;
-    }
-
-    /*
-		Label the data
-    You could also use a data-* attribute and content for this. That way "bloats" the HTML, 
-    this way means you need to keep HTML and CSS in sync.
-		*/
   }
 `;
 
