930ee3d184dadbbbed926296ac021d690482120a
Raymel Manreza <raymel.manreza@gmail.com>
2021-01-29 11:44:43 -0500

    test

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="portfolio-083">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
