9802ddd9fc8594a870f10cd227a2a95652de92df
Raymel Manreza <raymel.manreza@gmail.com>
2021-02-11 16:02:56 -0500

    Card page created. Card component initialized for dev.

---
+++ src/Components/Elements/Card/Card.styled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+interface styledCardProps {}
+
+export const StyledCard = styled.div``;
---
+++ src/Components/Elements/Card/Card.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledCard } from "./Card.styled";
+
+interface props {}
+
+const Card: React.FC = (props) => {
+  return <StyledCard>Test</StyledCard>;
+};
+
+export default Card;
