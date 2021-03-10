7eb1d1ce05c00d2ebe885de8c70f73c7924dd63d
Raymel Manreza <raymel.manreza@gmail.com>
2021-03-10 14:30:33 -0500

    Settings preview image and submit successfully

--- src/Components/Pages/Settings/Settings.tsx
+++ src/Components/Pages/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   StyledSettings,
   StyledProfile,
@@ -9,40 +9,35 @@ import {
 import PasswordReset from "../../Auth/PasswordReset/PasswordReset";
 import { Formik, Form, Field } from "formik";
 import Button from "../../Elements/Button/Button";
-import usrImg from "../../../Images/blank-profile-picture.png";
+import defaultImg from "../../../Images/blank-profile-picture.png";
 //import Dropdown from "../../Elements/Dropdown/Dropdown";
 
-export interface SettingsProps {
-  img?: File;
+export interface props {
+  user: user;
 }
 
-const Settings: React.FC<SettingsProps> = (props) => {
-  const [user, setUser] = useState({
-    uid: "",
-    img: "",
-    name: "",
-    meta: "",
-    email: "",
-    location: "",
-    social: {
-      twitter: "",
-      facebook: "",
-      instagram: "",
-    },
-    skills: "",
-    bio: "",
-  });
-  const [file, setFile] = useState("");
+interface user {
+  uid: string;
+  img?: string | any;
+  name: string;
+  meta?: string;
+  email: string;
+  location?: string;
+  social?: {
+    facebook?: string;
+    instagram?: string;
+    twitter?: string;
+  };
+  skills?: string;
+  bio?: string;
+}
+
+const Settings: React.FC<props> = (props) => {
+  const [user, setUser] = useState<user>(props.user);
+
   const [profileShow, setProfileShow] = useState(true);
   const [securityShow, setSecurityShow] = useState(false);
   const [themeShow, setThemeShow] = useState(false);
-
-  const imgHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFile(URL.createObjectURL(event.target.files[0]));
-      // setUser({...user, user.img: event.target.files[0]})
-    }
-  };
 
   const profileShowHandler = () => {
     setProfileShow(true);
@@ -61,23 +56,6 @@ const Settings: React.FC<SettingsProps> = (props) => {
     setSecurityShow(false);
     setProfileShow(false);
   };
-  const userImg = (
-    <div className="settings-usr-img">
-      {file === "" ? (
-        <img src={usrImg} alt="userImg" />
-      ) : (
-        <img src={file} alt="userImg" />
-      )}
-      <label>
-        <input
-          type="file"
-          id="upload-button"
-          onChange={(e) => imgHandleChange(e)}
-        />
-        Edit
-      </label>
-    </div>
-  );
 
   const nav = (
     <StyledNav>
@@ -111,36 +89,68 @@ const Settings: React.FC<SettingsProps> = (props) => {
   //   // />
   // );
 
-  const onSubmitHandler = (value: any) => {
+  const onSubmitHandler = (value: user) => {
     console.log(value);
+  };
+
+  const onChangePreview = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    let reader = new FileReader();
+
+    reader.onloadend = () => {
+      //  setImgPreview(reader.result);
+      setUser({ ...user, img: reader.result });
+    };
+    if (e.target.files) reader.readAsDataURL(e.target.files[0]);
   };
 
   const profile = (
     <StyledProfile show={profileShow}>
       <h3>Profile</h3>
-      {userImg}
       <Formik initialValues={user} onSubmit={onSubmitHandler}>
-        <Form>
-          <span>Name</span>
-          <Field id="name" name="name" type="name" placeholder="Your name" />
-          <span>Occupation</span>
-          <Field id="meta" name="meta" type="meta" placeholder="Title" />
-          <span>Email</span>
-          <Field
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Email address"
-          />
-          <span>Location</span>
-          <Field
-            id="location"
-            name="location"
-            type="location"
-            placeholder="Where are you?"
-          />
-          <span>Social Network</span>
-          {/* <Dropdown
+        {({ values, errors, setFieldValue }) => (
+          <Form>
+            <div className="settings-usr-img">
+              <label>
+                <input
+                  type="file"
+                  id="upload-button"
+                  onChange={(event) => {
+                    if (event.currentTarget.files) {
+                      setFieldValue("img", event.currentTarget.files[0]);
+                      onChangePreview(event);
+                    }
+                  }}
+                />
+                Edit
+              </label>
+              {user.img ? (
+                <img src={user.img} alt="userImg" />
+              ) : (
+                <img src={defaultImg} alt="defaultImg" />
+              )}
+            </div>
+            <span>Name</span>
+            <Field id="name" name="name" type="name" placeholder="Your name" />
+            <span>Occupation</span>
+            <Field id="meta" name="meta" type="meta" placeholder="Title" />
+            <span>Email</span>
+            <Field
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email address"
+            />
+            <span>Location</span>
+            <Field
+              id="location"
+              name="location"
+              type="location"
+              placeholder="Where are you?"
+            />
+            <span>Social Network</span>
+            {/* <Dropdown
             multiple
             fluid
             options={socialNetworkOptions}
@@ -148,32 +158,33 @@ const Settings: React.FC<SettingsProps> = (props) => {
             onChange={(value) => onChangeDropdown(value)}
           />
           {valueArray[0] && socialNetworksInputs} */}
-          <Field
-            id="twitter"
-            name="social.twitter"
-            type="twitter"
-            placeholder="Twitter"
-          />
-          <Field
-            id="facebook"
-            name="social.facebook"
-            type="facebook"
-            placeholder="Facebook"
-          />
-          <Field
-            id="instagram"
-            name="social.instagram"
-            type="instagram"
-            placeholder="Instagram"
-          />
-          <span>Skills</span>
-          <Field as="textarea" name="skills" />
-          <span>Bio</span>
-          <Field as="textarea" name="bio" />
-          <Button primary type="submit">
-            Update profile
-          </Button>
-        </Form>
+            <Field
+              id="twitter"
+              name="social.twitter"
+              type="twitter"
+              placeholder="Twitter"
+            />
+            <Field
+              id="facebook"
+              name="social.facebook"
+              type="facebook"
+              placeholder="Facebook"
+            />
+            <Field
+              id="instagram"
+              name="social.instagram"
+              type="instagram"
+              placeholder="Instagram"
+            />
+            <span>Skills</span>
+            <Field as="textarea" name="skills" />
+            <span>Bio</span>
+            <Field as="textarea" name="bio" />
+            <Button primary type="submit">
+              Update profile
+            </Button>
+          </Form>
+        )}
       </Formik>
     </StyledProfile>
   );
--- src/Pages/SettingsPage.tsx
+++ src/Pages/SettingsPage.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import ContentBox from "../Components/UI/ContentBox/ContentBox";
 import Settings from "../Components/Pages/Settings/Settings";
+import nina from "../Images/Nina.jpeg";
 
 export interface SettingsPageProps {}
+
+const fakeUser = {
+  uid: "4636ebfa-8039-11eb-9439-0242ac130002",
+  img: nina,
+  name: "Raymel Manreza",
+  meta: "React developer",
+  email: "raymel.manreza@gmail.com",
+  location: "Miami",
+  social: {
+    twitter: "@rmanreza",
+    facebook: "raymel.manreza",
+    instagram: "rmanreza",
+  },
+  skills: "React, HTML, CSS",
+  bio: "Just me",
+};
 
 const SettingsPage: React.FC<SettingsPageProps> = () => {
   return (
     <ContentBox title="Settings">
-      <Settings />
+      <Settings user={fakeUser} />
     </ContentBox>
   );
 };
