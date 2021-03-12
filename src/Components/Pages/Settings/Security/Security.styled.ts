6b13f859a8e2ff73043730de8190defdac82c5fa
Raymel Manreza <raymel.manreza@gmail.com>
2021-03-12 18:43:03 -0500

    Settings security tab work in progress

--- src/Components/Pages/Settings/Security/Security.styled.ts
+++ src/Components/Pages/Settings/Security/Security.styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "../../../../shared/Theme";
 
 interface StyledSecurityProps {
   show: boolean;
@@ -6,4 +7,28 @@ interface StyledSecurityProps {
 
 export const StyledSecurity = styled.div<StyledSecurityProps>`
   display: ${(props: StyledSecurityProps) => (props.show ? "block" : "none")};
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    margin: 0.2rem;
+    min-width: 15rem;
+
+    input {
+      min-width: 20rem;
+      height: 1.2rem;
+      padding: 0.2rem;
+      margin: 0.5rem;
+      border: 1px solid ${theme.disabledColor};
+      border-radius: 0.3rem;
+      //border-bottom: 1px solid ${theme.disabledColor};
+      :focus {
+        outline: none;
+        ::placeholder {
+          opacity: 0;
+        }
+      }
+    }
+  }
 `;
--- src/Components/Pages/Settings/Security/Security.tsx
+++ src/Components/Pages/Settings/Security/Security.tsx
@@ -1,14 +1,57 @@
 import React from "react";
 import { StyledSecurity } from "./Security.styled";
+import { Formik, Form, Field } from "formik";
 
 interface props {
   show: boolean;
 }
 
 const Security: React.FC<props> = (props) => {
+  const initialValues = {
+    currentPassword: "",
+    newPassword: "",
+    ConfirmNewPassword: "",
+  };
+
+  const onSubmitHandler = (value: any) => {
+    console.log(value);
+  };
+
   const security = (
     <StyledSecurity show={props.show}>
       <h3>Security</h3>
+      <Formik
+        initialValues={initialValues}
+        //validationSchema={SettingsSchema}
+        onSubmit={onSubmitHandler}
+      >
+        <Form>
+          <span>Current Password</span>
+          <Field
+            id="password"
+            name="currentPassword"
+            type="password"
+            //value={values.password}
+            placeholder=""
+          />
+          <span>New password</span>
+          <Field
+            id="password"
+            name="newPassword"
+            type="password"
+            //value={values.password}
+            placeholder=""
+          />
+          <span>Confirm new password</span>
+          <Field
+            id="password"
+            name="ConfirmNewPassword"
+            type="password"
+            //value={values.password}
+            placeholder=""
+          />
+        </Form>
+      </Formik>
     </StyledSecurity>
   );
   return security;
