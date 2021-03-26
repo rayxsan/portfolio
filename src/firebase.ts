088f099c6fcf735239ad6a0c0a71dab673a9b669
Raymel Manreza <raymel.manreza@gmail.com>
2021-03-26 09:26:10 -0400

    deploy

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDu6dx-Nz8s4Q-V_uvpQ0BJki9F3JJS8MY",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
